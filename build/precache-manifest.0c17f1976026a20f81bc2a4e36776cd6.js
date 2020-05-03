4a28c6df0cbb3057d96f014e8ca53e3650d90bbe
Lucian Iernye <lucian.iernye@gmail.com>
2020-05-03 19:57:35 +0100

    https

---
+++ build/precache-manifest.0c17f1976026a20f81bc2a4e36776cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbd2c063d45c03434dcf5da641ea352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4b463c969db48631258",
+    "url": "/static/css/main.cd615071.chunk.css"
+  },
+  {
+    "revision": "1603fa7fd1c2c3c1e0f2",
+    "url": "/static/js/2.9c1ee214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c1ee214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b463c969db48631258",
+    "url": "/static/js/main.eb4cf0fd.chunk.js"
+  },
+  {
+    "revision": "b066e09717e622388a0a",
+    "url": "/static/js/runtime-main.1cf72db6.js"
+  }
+]);
